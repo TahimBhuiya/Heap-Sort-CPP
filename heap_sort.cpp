41df4d9ba2627a508000aa4f26fdb366427db122
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-10-20 18:28:25 -0400

    Added onto heapify function

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -7,7 +7,7 @@ void heapify(int arr[], int n, int i) {
     int largest = i;       // Initialize largest as root
 
     int left = 2 * i + 1;  // Left child index
-     
+    int right = 2 * i + 2; 
 
     
     
