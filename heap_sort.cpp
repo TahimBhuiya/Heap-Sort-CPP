b0a737792c90b066c250678c5769e6c296a78646
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-10-22 19:55:09 -0400

    Added onto heapSort function

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -28,7 +28,7 @@ void heapify(int arr[], int n, int i) {
 void heapSort(int arr[], int n) {
     // Build a max-heap
     for (int i = n / 2 - 1; i >= 0; i--)
-        
+        heapify(arr, n, i);
 
     
     
